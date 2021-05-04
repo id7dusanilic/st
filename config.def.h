869cefbc9358afe4003ab87df9f079f10ceb7c0d
Dusan Ilic <idtronicsrs@gmail.com>
2021-05-04 20:55:34 +0200

    Font changed

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack:pixelsize=15:antialias=true:autohint=true";
+static char *font = "Mononoki Nerd Font:size=15:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
